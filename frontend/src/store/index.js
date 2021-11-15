a7236db9486d536455d71761d1cffcc869b73a89
Last-Vega <watanabe.shingo.ss@alumni.tsukuba.ac.jp>
2021-11-15 20:49:02 +0900

    Update index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,10 +2,33 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-
+const initialState = {
+  loginState: {
+    id: '',
+    token: '',
+    isLogin: false
+  }
+}
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: initialState,
+  mutations: {
+    updateLoginState(state, data) {
+      state.loginState.id = data.id
+      state.loginState.token = data.token
+      if (state.loginState.token) {
+        console.log('works')
+        state.loginState.isLogin = true
+        console.log(state.loginState.isLogin)
+      }
+    },
+    logout(state, isLogout) {
+      if (isLogout) {
+        state.loginState.userId = ''
+        state.loginState.userToken = ''
+        state.loginState.isLogin = false
+      }
+    }
+  },
   actions: {},
   modules: {}
 })
