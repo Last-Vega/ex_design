2b4ea0a42286c4c1fc51cd457b64e6252fdcc80f
Last-Vega <watanabe.shingo.ss@alumni.tsukuba.ac.jp>
2021-11-17 13:16:44 +0900

    永続化

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 const initialState = {
@@ -30,5 +31,6 @@ export default new Vuex.Store({
     }
   },
   actions: {},
-  modules: {}
+  modules: {},
+  plugins: [createPersistedState({ storage: window.sessionStorage })]
 })
