f2791a6f3dbe03e4160bd36b1bbc7aef6bc9af00
Last-Vega <watanabe.shingo.ss@alumni.tsukuba.ac.jp>
2021-11-22 13:37:26 +0900

    a

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   outputDir: '../docs',
   transpileDependencies: ['vuetify'],
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/ex_design/'
-    : '/'
+  assetsDir: './',
+  publicPath: './'
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/ex_design/'
+  //   : '/'
 }
