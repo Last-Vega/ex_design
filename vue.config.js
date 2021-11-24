bd12a956cc913e532b73382ea1acf16921d9c5ff
Last-Vega <watanabe.shingo.ss@alumni.tsukuba.ac.jp>
2021-11-24 12:23:16 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   outputDir: 'docs',
   transpileDependencies: ['vuetify'],
-  assetsDir: './'
+  assetsDir: './',
   // publicPath: '/ex_design/'
-  // publicPath: process.env.NODE_ENV === 'production' ? '/ex_design/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/ex_design/' : '/'
 }
