6e1696a8ad067efaa798bb12824b1b2c81759ef5
Last-Vega <watanabe.shingo.ss@alumni.tsukuba.ac.jp>
2021-11-26 15:20:44 +0900

    Update LatentSpace.vue

--- frontend/src/views/LatentSpace.vue
+++ frontend/src/views/LatentSpace.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
     <DisplayLatentSpace />
-    <!-- <section><Instraction /></section> -->
   </div>
 </template>
 
 <script>
 import DisplayLatentSpace from '@/components/displayLatentInfo'
-// import Instraction from '../components/instraction.vue'
 export default {
   name: 'LatentSpace',
   components: {
     DisplayLatentSpace
-    // Instraction
   },
   data () {
     return {}
